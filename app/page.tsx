4b9edce770684e1eb1163d20d37f14d69ca5737d
Quinn Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-10-11 10:29:43 -0400

    Test deploy 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 "use client";
 import Home from "./home/page";
 import Navbar from "./components/navbar";
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 
 export default function Start() {
   const navLinks = ["Home", "The Band", "Listen", "Tour", "Gallery"];
 
-  const theme = createTheme({
-    palette: {
-      secondary: {
-        main: "#6FF78B",
-      },
-    },
-  });
-
   return (
-    <MuiThemeProvider theme={theme}>
-      <main className="">
-        <Home />
-        {/* <Navbar /> */}
-      </main>
-    </MuiThemeProvider>
+    <main className="">
+      <Home />
+      {/* <Navbar /> */}
+    </main>
   );
 }
